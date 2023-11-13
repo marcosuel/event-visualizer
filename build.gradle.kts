2a61928f15df91515eec110af80f23befac444e1
Marcos Suel <marcosuelns@gmail.com>
2023-11-12 23:41:24 -0300

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+	java
+	id("org.springframework.boot") version "2.7.18-SNAPSHOT"
+	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+}
+
+group = "br.com.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+	maven { url = uri("https://repo.spring.io/milestone") }
+	maven { url = uri("https://repo.spring.io/snapshot") }
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-websocket")
+	implementation("org.springframework.kafka:spring-kafka")
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.kafka:spring-kafka-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+//tasks.bootBuildImage {
+//	builder.set("paketobuildpacks/builder-jammy-base:latest")
+//}
---
+++ src/main/java/br/com/example/eventVisualizer/EventVisualizerApplication.java
@@ -0,0 +1,13 @@
+package br.com.example.eventVisualizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventVisualizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventVisualizerApplication.class, args);
+	}
+
+}
