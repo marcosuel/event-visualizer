bc1a3f8c39cea2beea00b76f26c5d1435c5d4399
Marcos Suel <marcosuelns@gmail.com>
2023-11-19 01:29:38 -0300

    adiciona websocket para enviar eventos recebidos do kafka ao front

--- src/main/java/br/com/example/eventvisualizer/service/KafkaListenerService.java
+++ src/main/java/br/com/example/eventvisualizer/service/KafkaListenerService.java
@@ -7,8 +7,11 @@ import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerEndpoint;
 import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
 import org.springframework.kafka.config.MethodKafkaListenerEndpoint;
+import org.springframework.kafka.listener.MessageListenerContainer;
 import org.springframework.messaging.handler.annotation.support.DefaultMessageHandlerMethodFactory;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 @RequiredArgsConstructor
@@ -16,27 +19,28 @@ public class KafkaListenerService {
 
     private final KafkaListenerEndpointRegistry kafkaListenerEndpointRegistry;
     private final KafkaListenerContainerFactory kafkaListenerContainerFactory;
+//    private final SimpMessagingTemplate messagingTemplate;
+    private final KafkaTemplateListener kafkaTemplateListener;
     String kafkaGroupId = "kafkaGroupId";
-    static final String LISTENER_ID = "kafkaListenerId-local";
+    private static final String LISTENER_PREFIX = "kafkaListenerId-local-";
+    private final AtomicLong listenerIdSuffix = new AtomicLong(1);
 
     public void registerListener(String topic) {
+        destroyActiveListeners();
         kafkaListenerEndpointRegistry.registerListenerContainer(
                 createKafkaListenerEndpoint(topic),
                 kafkaListenerContainerFactory,
                 true
         );
-        unregisterListeners();
     }
 
-    private void unregisterListeners() {
-        kafkaListenerEndpointRegistry.getListenerContainerIds().forEach(
-                kafkaListenerEndpointRegistry::unregisterListenerContainer
-        );
+    private void destroyActiveListeners() {
+        kafkaListenerEndpointRegistry.getListenerContainers().forEach(MessageListenerContainer::destroy);
     }
 
     private KafkaListenerEndpoint createKafkaListenerEndpoint(String topic) {
         MethodKafkaListenerEndpoint<String, String> kafkaListenerEndpoint = createDefaultMethodKafkaListenerEndpoint(topic);
-        kafkaListenerEndpoint.setBean(new KafkaTemplateListener());
+        kafkaListenerEndpoint.setBean(kafkaTemplateListener);
         try {
             kafkaListenerEndpoint.setMethod(KafkaTemplateListener.class.getMethod("onMessage", ConsumerRecord.class));
         } catch (NoSuchMethodException e) {
@@ -47,7 +51,7 @@ public class KafkaListenerService {
 
     private MethodKafkaListenerEndpoint<String, String> createDefaultMethodKafkaListenerEndpoint(String topic) {
         MethodKafkaListenerEndpoint<String, String> kafkaListenerEndpoint = new MethodKafkaListenerEndpoint<>();
-        kafkaListenerEndpoint.setId(LISTENER_ID);
+        kafkaListenerEndpoint.setId(LISTENER_PREFIX + listenerIdSuffix.getAndIncrement());
         kafkaListenerEndpoint.setAutoStartup(true);
         kafkaListenerEndpoint.setTopics(topic);
         kafkaListenerEndpoint.setMessageHandlerMethodFactory(new DefaultMessageHandlerMethodFactory());
